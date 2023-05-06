24ad32813240e4d63b65e9a52e4a0474b26c5452
chauhoang <chauhoang@chauhoangs-MacBook-Pro.local>
2023-05-06 16:20:26 +0700

    game
    game

---
+++ game/game/Config.h
@@ -0,0 +1,37 @@
+#pragma once
+#pragma once
+
+//init window
+#define WINDOW_CLASS_NAME L"ContraWindow"
+#define MAIN_WINDOW_TITLE L"Contra"
+#define FPS 60
+#define MAX_FRAME_RATE 1000.0f/ 60
+
+#define BACKGROUND_COLOR D3DCOLOR_XRGB(255,255,255)
+#define SCREEN_WIDTH 256
+#define SCREEN_HEIGHT 256
+
+//starting sceen
+#define KEY_SELECT_OPTION VK_RETURN
+#define TEXT_OPTION_PLAY "PLAY GAME"
+#define TEXT_OPTION_HELP "HELP"
+#define TEXT_OPTION_OPTION "OPTION"
+#define TEXT_OPTION_HISCORE "HIGHEST SCORE"
+#define TEXT_OPTION_QUIT "QUIT"
+
+//Sound
+
+// Backgound Path 
+#define PATH_BACKGOUND_SS "Resources\\BackGround\\SSBackGround.png"
+#define PATH_BACKGOUND_GOS "Resources\\BackGround\\GOSBackGround.png"
+#define PATH_BACKGOUND_OS "Resources\\BackGround\\OSBackGround.png"
+#define PATH_BACKGOUND_OSCURSOR "Resources\\BackGround\\OScursor.png"
+#define PATH_BACKGOUND_LSS1 "Resources\\BackGround\\LSBackGroundStage1.png"
+#define PATH_BACKGOUND_LSS2 "Resources\\BackGround\\LSBackGroundStage2.png"
+#define PATH_BACKGOUND_LSS3 "Resources\\BackGround\\LSBackGroundStage3.png"
+
+//Font Path
+#define PATH_FONT_GAME "Resources\\Fonts\\font.png"
+#define PATH_FONT_AA "Resources\\Fonts\\fontAA.png"
+
+//Map Path
