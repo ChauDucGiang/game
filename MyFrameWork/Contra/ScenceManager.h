340a509ba717f2cfa1f8ac5663bd37c633ef8947
ChauDucGiang <chau.ducgiang@gmail.com>
2023-05-09 13:59:52 +0700

    Scence
    Scence

---
+++ MyFrameWork/Contra/ScenceManager.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include "Scene.h"
+
+class SceneManager
+{
+private:
+	static SceneManager* _instance;				// Instance (Singleton Pattern)
+
+public:
+	Scene* CurScene;							// Scene hiện tại mà Manager đang giữ
+	SceneManager();
+	void ReplaceScene(Scene* scene);			// Thay thế Scene hiện có bằng Scene khác (dùng cho chuyển cảnh)
+	Scene* GetCurScene() { return CurScene; };	// Get Current Scene
+	static SceneManager* GetInstance();			// Get Instance (Singleton Pattern)
+};
---
+++ MyFrameWork/Contra/SoundManager.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include "Sound.h"
+
+// Chứa tất cả các Sound cần dùng cho Game
+// Dùng Singleton Pattern
+
+class SoundManager
+{
+private:
+	static class SoundManager* _instance;
+
+public:
+	void LoadResources();
+	Sound* GetSound(char name);
+};
